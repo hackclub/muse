2539894ae425d23346751db80d1f02bbb130ea60
leomcelroy <leomcelroy@gmail.com>
2021-10-14 15:29:29 -0400

    reorganization

--- examples/starter.js
+++ examples/starter.js
@@ -1,6 +1,25 @@
+// song goes here
+createMuse({ bpm: 110, type: "sin" }).play(
+`
+
+
+`
+)
+
+
 // below maps keys so you can compose
 const key = 4
 const type = "triangle" // sine | triangle | square | sawtooth
+
+// const KeyA = () => createMuse({ type }).play(`clay`)
+// const KeyS = () => createMuse({ type }).play(`bubbles`)
+// const KeyD = () => createMuse({ type }).play(`flash1`)
+// const KeyF = () => createMuse({ type }).play(`moon`)
+// const KeyG = () => createMuse({ type }).play(`pinwheel`)
+// const KeyH = () => createMuse({ type }).play(`splits`)
+// const KeyJ = () => createMuse({ type }).play(`strike`)
+// const KeyK = () => createMuse({ type }).play(`wipe`)
+// const KeyL = () => createMuse({ type }).play(`ufo`)
 
 const KeyA = () => createMuse({ type }).play(`a${key}`)
 const KeyS = () => createMuse({ type }).play(`b${key}`)
