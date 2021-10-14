693ca6c3e3beb9b4f768dae0b9c2633d89111e89
leomcelroy <leomcelroy@gmail.com>
2021-10-14 15:35:42 -0400

    Update defaultProg.js

--- defaultProg.js
+++ defaultProg.js
@@ -1,10 +1,10 @@
 export const defaultProg = `
 // song goes here
 createMuse({ bpm: 110, type: "sin" }).play(
-`
+\`
 
 
-`
+\`
 )
 
 
@@ -12,15 +12,15 @@ createMuse({ bpm: 110, type: "sin" }).play(
 const key = 4
 const type = "triangle" // sine | triangle | square | sawtooth
 
-const KeyA = () => createMuse({ type }).play(`a${key}`)
-const KeyS = () => createMuse({ type }).play(`b${key}`)
-const KeyD = () => createMuse({ type }).play(`c${key+1}`)
-const KeyF = () => createMuse({ type }).play(`d${key+1}`)
-const KeyG = () => createMuse({ type }).play(`e${key+1}`)
-const KeyH = () => createMuse({ type }).play(`f#${key+1}`)
-const KeyJ = () => createMuse({ type }).play(`g${key+1}`)
-const KeyK = () => createMuse({ type }).play(`a${key+1}`)
-const KeyL = () => createMuse({ type }).play(`b${key+1}`)
+const KeyA = () => createMuse({ type }).play(\`a\${key}\`)
+const KeyS = () => createMuse({ type }).play(\`b\${key}\`)
+const KeyD = () => createMuse({ type }).play(\`c\${key+1}\`)
+const KeyF = () => createMuse({ type }).play(\`d\${key+1}\`)
+const KeyG = () => createMuse({ type }).play(\`e\${key+1}\`)
+const KeyH = () => createMuse({ type }).play(\`f#\${key+1}\`)
+const KeyJ = () => createMuse({ type }).play(\`g\${key+1}\`)
+const KeyK = () => createMuse({ type }).play(\`a\${key+1}\`)
+const KeyL = () => createMuse({ type }).play(\`b\${key+1}\`)
 
 // these returned keys get bound
 return { 
