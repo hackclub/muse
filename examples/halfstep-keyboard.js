241f99e7bf1f475bddc267904c29febbb2a85170
leomcelroy <leomcelroy@gmail.com>
2021-10-26 17:19:07 -0400

    updated samples

--- examples/halfstep-keyboard.js
+++ examples/halfstep-keyboard.js
@@ -2,19 +2,19 @@ const key = 4
 const type = "triangle" // sine | triangle | square | sawtooth
 
 // a | a# | b | c | c# | d | d# | e | f | f# | g | g#
-const q = () => createMuse({ type }).play(`a${key}`)
-const w = () => createMuse({ type }).play(`a#${key}`)
-const e = () => createMuse({ type }).play(`b${key}`)
-const r = () => createMuse({ type }).play(`c${key+1}`)
-const t = () => createMuse({ type }).play(`c#${key+1}`)
-const y = () => createMuse({ type }).play(`d${key+1}`)
-const u = () => createMuse({ type }).play(`d#${key+1}`)
-const i = () => createMuse({ type }).play(`e${key+1}`)
-const o = () => createMuse({ type }).play(`f${key+1}`)
-const p = () => createMuse({ type }).play(`f#${key+1}`)
-const leftBracket = () => createMuse({ type }).play(`g${key+1}`)
-const rightBracket = () => createMuse({ type }).play(`g#${key+1}`)
-const backslash = () => createMuse({ type }).play(`a${key+1}`)
+const q = () => createMuse({ type }).play`a${key}`
+const w = () => createMuse({ type }).play`a#${key}`
+const e = () => createMuse({ type }).play`b${key}`
+const r = () => createMuse({ type }).play`c${key+1}`
+const t = () => createMuse({ type }).play`c#${key+1}`
+const y = () => createMuse({ type }).play`d${key+1}`
+const u = () => createMuse({ type }).play`d#${key+1}`
+const i = () => createMuse({ type }).play`e${key+1}`
+const o = () => createMuse({ type }).play`f${key+1}`
+const p = () => createMuse({ type }).play`f#${key+1}`
+const leftBracket = () => createMuse({ type }).play`g${key+1}`
+const rightBracket = () => createMuse({ type }).play`g#${key+1}`
+const backslash = () => createMuse({ type }).play`a${key+1}`
 
 // these returned s get bound
 bindKeys({ 
--- examples/starter.js
+++ examples/starter.js
@@ -1,27 +1,26 @@
 // song goes here
-createMuse({ bpm: 120, type: "sine" }).play(
-`
+createMuse({ bpm: 120, type: "sine" }).play`
 
 
 
 
 `
-)
+
 
 
 // below maps keys so you can compose
 const key = 4
 const type = "triangle" // sine | triangle | square | sawtooth
 
-const a = () => createMuse({ type }).play(`a${key}`)
-const s = () => createMuse({ type }).play(`b${key}`)
-const d = () => createMuse({ type }).play(`c${key+1}`)
-const f = () => createMuse({ type }).play(`d${key+1}`)
-const g = () => createMuse({ type }).play(`e${key+1}`)
-const h = () => createMuse({ type }).play(`f#${key+1}`)
-const j = () => createMuse({ type }).play(`g${key+1}`)
-const k = () => createMuse({ type }).play(`a${key+1}`)
-const l = () => createMuse({ type }).play(`b${key+1}`)
+const a = () => createMuse({ type }).play`a${key}`
+const s = () => createMuse({ type }).play`b${key}`
+const d = () => createMuse({ type }).play`c${key+1}`
+const f = () => createMuse({ type }).play`d${key+1}`
+const g = () => createMuse({ type }).play`e${key+1}`
+const h = () => createMuse({ type }).play`f#${key+1}`
+const j = () => createMuse({ type }).play`g${key+1}`
+const k = () => createMuse({ type }).play`a${key+1}`
+const l = () => createMuse({ type }).play`b${key+1}`
 
 // these returned s get bound
 bindKeys({ 
